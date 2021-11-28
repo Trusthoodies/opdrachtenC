82f1e000322881f63e00a9134217c3433ce8d551
Ericbratt <95029137+Ericbratt@users.noreply.github.com>
2021-11-28 17:31:40 +0100

    Add files via upload

---
+++ Les 2/Opdracht 1/Opdracht1B.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void print_function( char letter );
+int ctr;
+int counter;
+int mainopdracht( void )
+{
+char star = '*';
+char dash = '-';
+for (counter = 0 ; counter < 10; counter++ )
+{
+print_function( star );
+print_function( dash );
+printf("\n");
+}
+return 0;
+}
+void print_function ( char letter )
+{
+for ( ctr = 0; ctr < 10; ctr++) 
+{
+printf("%c", letter);
+}
+}
