f211a712569ce169e34c87a7b5a249801421ab8f
Ericbratt <95029137+Ericbratt@users.noreply.github.com>
2021-11-28 19:31:11 +0100

    Update Opdracht 2B.cpp

--- Les 2/Opdracht 2/Opdracht 2B.cpp
+++ Les 2/Opdracht 2/Opdracht 2B.cpp
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+void omtrek(float *gegevendiameter);
+
+
+int main()
+{
+    float diameter;
+    float *dia;
+    printf("Wat is de diameter van de cirkel?: ");
+    scanf("%f", &diameter);     
+    dia = &diameter;
+    omtrek(dia);
+    printf("%f\n", diameter);
+    return 0;
+}
+
+
+void omtrek(float *gegevendiameter)
+{
+    *gegevendiameter = *gegevendiameter * M_PI;
+}
